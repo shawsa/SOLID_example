ac57e73e1cd176ccad220b5df8196a2d1a3f958d
sage <sage.b.shaw@gmail.com>
2022-05-26 11:02:03 -0600

    preparing for live-refactoring

--- driver.py
+++ driver.py
@@ -16,17 +16,16 @@ Specifications of the assignment:
     Demonstrate correctness with the following
         Plot the exact and approximate solutions (error on semi-log)
 '''
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sym
-
-from time_domain import TimeDomain, TimeDomain_Start_Stop_MaxSpacing
-from time_integrator import EulerIntegrator, RK4_Integrator, AB2_Integrator
+from time_domain import TimeDomain, TimeDomain_Start_Spacing_Stop
+from time_integrator import Euler, RK4, AB2
 
 
 def get_order(e0, e1, h0, h1):
     return np.log(e1/e0)/np.log(h1/h0)
+
 
 def main():
 
@@ -34,6 +33,7 @@ def main():
     # manufactured solution
     ###################
     t_sym = sym.symbols('t')
+    u_sym = sym.symbols('u')
     u_true_sym = sym.sin(t_sym)
     u_true = sym.lambdify(t_sym, u_true_sym)
     rhs_sym = u_true_sym.diff('t')
@@ -48,21 +48,24 @@ def main():
     # Solution over time
     ###################
 
-    time = TimeDomain_Start_Stop_MaxSpacing(
-            start=0,
-            end=10,
-            max_spacing=.1)
-    solver = EulerIntegrator()
+    tf = 10
+    max_spacing = 0.1
+    time = TimeDomain_Start_Spacing_Stop(t0, max_spacing, tf)
+    print(f'Rectifying time step to {time.spacing} from {max_spacing}')
+    # solver = Euler()
+    # solver = RK4()
+    # solver = AB2(Euler(), 2)
+    solver = AB2(RK4(), 1)
     ts, us = solver.solve(u0, rhs, time)
 
     fig, axes = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
 
-    axes[0].plot(time.array, u_true(ts), 'b-', label='exact')
-    axes[0].plot(time.array, us, 'g-', label='approximate')
+    axes[0].plot(ts, u_true(ts), 'b-', label='exact')
+    axes[0].plot(ts, us, 'g-', label='approximate')
     axes[0].legend()
     axes[0].set_xlabel('$t$')
 
-    axes[1].semilogy(time.array, (us - u_true(ts)), 'k-', label='error')
+    axes[1].semilogy(ts, (us - u_true(ts)), 'k-', label='error')
     axes[1].legend()
     plt.suptitle('Solution over time.')
     plt.show()
@@ -72,42 +75,31 @@ def main():
     ###################
 
     tf = 1_000
-    dts = [.9 * 2**(-i) for i in range(1, 6)]
+    max_dts = [.9 * 2**(-i) for i in range(1, 6)]
     error_dict = {}
-    solvers = [
-            EulerIntegrator(),
-            RK4_Integrator(),
-            AB2_Integrator(EulerIntegrator(), 2),
-            AB2_Integrator(RK4_Integrator(), 1),
-            AB2_Integrator(
-                AB2_Integrator(
-                    EulerIntegrator(),
-                    seed_step=1),
-                seed_step=1)  # Lol
-        ]
-    for solver in solvers:
+    for solver in [Euler(),
+                   RK4(),
+                   AB2(Euler(), 2),
+                   AB2(AB2(Euler(), 2), 2)]:
         errors = []
-        for max_dt in dts:
-            time = TimeDomain_Start_Stop_MaxSpacing(
-                        start=t0,
-                        end=tf,
-                        max_spacing=max_dt)
+        for max_dt in max_dts:
+            time = TimeDomain_Start_Spacing_Stop(t0, max_dt, tf)
             uf = solver.t_final(u0, rhs, time)
             errors.append(uf - u_true(tf))
-        error_dict[solver.name] = errors.copy()
+        error_dict[str(solver)] = errors.copy()
 
-    plt.figure('Convergence')
+    # fig = plt.figure('Convergence')
 
     for method, errors in error_dict.items():
-        order = get_order(errors[-2], errors[-1], dts[-2], dts[-1])
+        order = get_order(errors[-2], errors[-1],
+                          max_dts[-2], max_dts[-1])
         order_str = f' $\\mathcal{{O}}({order:.1f})$'
-        plt.loglog(dts, errors, '.-', label=method + order_str)
+        plt.loglog(max_dts, errors, '.-', label=method + order_str)
 
     plt.xlabel(r'$\Delta_t$')
     plt.ylabel('Error')
     plt.legend()
     plt.show()
-
 
 if __name__ == '__main__':
     main()
